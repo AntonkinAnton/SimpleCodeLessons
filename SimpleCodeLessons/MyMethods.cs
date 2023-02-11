71e95dd80b973d277150b7ed133f65fb5f9b1ac7
AntonAntonkin <antonkin_anton@mail.ru>
2023-02-11 23:14:39 +0500

    Добавьте файлы проекта.

---
+++ SimpleCodeLessons/MyMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCodeLessons
+{
+    public class MyMethods
+    {
+        public static int GetNum(string message)
+        {
+            int num;
+            do
+            {
+                System.Console.WriteLine(message);
+            } while (!int.TryParse(Console.ReadLine(), out num));
+            return num;
+        }
+
+        public static void PrintArray(int[] array, string message)
+        {
+            System.Console.WriteLine(message);
+            for (int i = 0; i < array.Length; i++)
+            {
+
+
+                System.Console.Write("{0,3}", array[i]);
+
+                
+            }
+            
+        }
+
+
+
+
+    }
+}
---
+++ SimpleCodeLessons/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCodeLessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = MyMethods.GetNum("Введите X");
+
+        }
+
+    }
+}
