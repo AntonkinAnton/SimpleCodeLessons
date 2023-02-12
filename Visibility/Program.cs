20cca7763b321486c1ecb375e901efc7d90e9df1
AntonAntonkin <antonkin_anton@mail.ru>
2023-02-13 00:51:00 +0500

    Visibility added

---
+++ Visibility/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Visibility.MyMethods;
+
+namespace Visibility
+{
+    internal class Program
+    {
+        static string repeatingValue = "Первый раз объявлена";
+
+        static void SecondTimeDeclare ()
+        {
+            string repeatingValue = "Второй раз объявлена";
+            Console.WriteLine(repeatingValue);
+
+       
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Program.repeatingValue);
+
+            SecondTimeDeclare();
+
+            string repeatingValue = "Третий раз объявлена";
+
+            Console.WriteLine(repeatingValue);
+
+            End();
+
+            
+
+
+                
+
+
+
+          
+        }
+    }
+}
