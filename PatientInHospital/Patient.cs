b89c38abc2876aa047637ec530a883e717682bfc
AntonAntonkin <antonkin_anton@mail.ru>
2023-03-01 04:50:00 +0500

    Patient task add

---
+++ PatientInHospital/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientInHospital
+{
+    internal class Patient
+    {
+        public string name;
+        public int age;
+        public Gender gender;
+        public string adress;
+        
+        public enum Gender
+        {
+            Male = 0,
+            Female = 1,
+            Other = 2
+        }
+
+        public Patient(string name = "Ivanov Ivan", int age = 20, Gender gender = Gender.Male, string adress = "selo V.Perduncy" )
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.adress = adress;
+            
+        }
+
+        public void PrintPatientInfo ()
+        {
+            Console.WriteLine($"Name: {name}\nAge: {age}\nGender: {gender}\nAdress: {adress}\n");
+            Console.WriteLine("------------------------\n");
+        }
+    }
+}
---
+++ PatientInHospital/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PatientInHospital
+{
+    internal class Program
+    {
+        public static List<Patient> MakeListFromInfo(string[] arrayNames, int[]arrayAges, Patient.Gender[]arrayGender, string[]arrayAdress)
+        {
+            List<Patient> patients = new List<Patient>();
+
+            for (int i = 0; i < arrayNames.Length; i++)
+            {
+                patients.Add(new Patient(arrayNames[i], arrayAges[i], arrayGender[i], arrayAdress[i]));
+            }
+
+            return patients;
+        }
+        //Хотите спросить "накой хера тут метод для наполнения листа и эти сраные массивы? Да потому что могу ¯\_(ツ)_/¯
+        static void Main(string[] args)
+        {
+            string[] namesArray = { "John Pindoss", "Vera Pavlovich", "Delphi Basicov" };
+            int[] agesArray = { 25, 41, 37 };
+            string[] adressesArray = { "GodBlessAmerica street", "Goose-crystal city", "Silicon Valley" };
+            Patient.Gender[] gendersArray = { Patient.Gender.Male, Patient.Gender.Female, Patient.Gender.Other };
+
+            List<Patient> patientsList = MakeListFromInfo(namesArray, agesArray, gendersArray, adressesArray);
+
+            patientsList.Add(new Patient());
+
+            foreach (var patient in patientsList)
+            {
+                patient.PrintPatientInfo();
+            }
+            
+        }
+    }
+}
