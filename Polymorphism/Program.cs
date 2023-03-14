4aec1aa7612905cc18110d4ccd23ef99073509c7
AntonkinAnton <antonkin_anton@mail.ru>
2023-03-14 20:33:13 +0500

    changed classes types to var

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -7,17 +7,17 @@ namespace Polymorphism
     {
         static void Main()
         {
-            Circle circle = new Circle(GetDouble("Введите радиус круга:"));
+            var circle = new Circle(GetDouble("Введите радиус круга:"));
             ShowAreaOfShape(circle);
             Console.ReadKey(); 
             Console.Clear();
 
-            Square square = new Square(GetDouble("Введите сторону квадрата:"));
+            var square = new Square(GetDouble("Введите сторону квадрата:"));
             ShowAreaOfShape(square);
             Console.ReadKey();
             Console.Clear();
 
-            Rectangle rectangle = new Rectangle(GetDouble("Введите первую сторону квадрата:"),
+            var rectangle = new Rectangle(GetDouble("Введите первую сторону квадрата:"),
                 GetDouble("Введите вторую сторону квадрата:"));
             ShowAreaOfShape(rectangle);
             Console.ReadKey();
